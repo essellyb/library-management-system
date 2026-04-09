71191306c1066f0850a753b1cfdeebe17d258ac3
david <mresselll2003@gmail.com>
2026-04-09 15:38:45 +0100

    Updated DTOs

--- mylibrary/src/main/java/com/essel/mylibrary/auth/RegisterRequest.java
+++ mylibrary/src/main/java/com/essel/mylibrary/auth/RegisterRequest.java
@@ -1,6 +1,9 @@
 package com.essel.mylibrary.auth;
 
 import com.essel.mylibrary.user.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Builder
@@ -10,10 +13,16 @@ import lombok.*;
 @Getter
 public class RegisterRequest {
 
+    @NotBlank(message = "Username field must not be empty")
+    @Size(min = 5, max = 20, message = "Username should be between 5 and 20 characters")
     private String username;
 
+    @NotBlank(message = "Email field must not be empty")
+    @Email(message = "Enter a valid email")
     private String email;
 
+    @NotBlank(message = "Password field must not be empty")
+    @Size(message = "Password must be more than 5 characters", min = 5 , max = 50)
     private String password;
 
     private Role role;
