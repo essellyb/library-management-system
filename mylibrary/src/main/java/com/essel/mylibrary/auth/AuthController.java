cb2deaa784b33162c7b02020eacd5e5e96276b09
david <mresselll2003@gmail.com>
2026-04-29 12:17:21 +0100

    added a delete-user and delete-admin endpoint and secured them with role authentications

--- mylibrary/src/main/java/com/essel/mylibrary/auth/AuthController.java
+++ mylibrary/src/main/java/com/essel/mylibrary/auth/AuthController.java
@@ -4,10 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -22,7 +19,7 @@ public class AuthController {
     }
 
     @PostMapping("/register-admin")
-    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('SUPER_ADMIN')")
     public ResponseEntity<AuthResponse> registerAdmin(@RequestBody RegisterRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(authService.registerAdmin(request));
     }
@@ -30,5 +27,19 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> authenticate(@RequestBody AuthRequest request) {
         return ResponseEntity.ok(authService.login(request));
+    }
+
+    @DeleteMapping("/delete-user/{id}")
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN', 'ADMIN')")
+    public ResponseEntity<Void> deleteUserById(@PathVariable Long id) {
+        authService.deleteUserById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/delete-admin/{id}")
+    @PreAuthorize("hasRole('SUPER_ADMIN')")
+    public ResponseEntity<Void> deleteAdminById(@PathVariable Long id) {
+        authService.deleteAdminById(id);
+        return ResponseEntity.noContent().build();
     }
 }
